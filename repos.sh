b1bce6441e0c4df09c6a2f3239f73298bfd7f66c
Facundo Perez <facundoperez94@gmail.com>
2024-01-10 16:42:36 -0300

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -4,7 +4,7 @@ DEPTH_DEFAULT=1
 ADDONS_PATH=./addons
 
 # ecovis bank management
-git clone https://github.com/xfacundoperez/ecovis-bank-management.git -b $ODOO_VERSION --depth $DEPTH_DEFAULT $ADDONS_PATH/ecovis-bank-management
+git clone https://github.com/xfacundoperez/odoo-addons-ac.git -b $ODOO_VERSION --depth $DEPTH_DEFAULT $ADDONS_PATH/odoo-addons-ac
 
 # oca-reporting-engine
 git clone https://github.com/oca/reporting-engine.git -b $ODOO_VERSION --depth $DEPTH_DEFAULT $ADDONS_PATH/oca-reporting-engine
